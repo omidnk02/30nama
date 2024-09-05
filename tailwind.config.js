e707c10fccc5e716a1b26aae9243e09864a6d2dd
omid <omiidnk02@gmail.com>
2024-09-05 03:46:45 +0330

    update 0.1

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaDemibold: "Dana Demibold",
+      },
+    },
+  },
+  plugins: [],
+};
